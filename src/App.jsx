65add8aabd4f499ff8a89bb2eab1390aa7e9745f
truongan-chiem <ch.tr.an2k@gmail.com>
2023-03-01 01:32:03 +0700

    change title

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { useEffect } from "react";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
 
 function App() {
+  useEffect(() => {
+    document.title = "Bikcraft"
+  }, [])
+  
   return (
     <div>
         <Header />
